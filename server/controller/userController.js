3e21a59363f4dd1a1a6a929f402c996e0f71ee2a
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-09 07:32:41 +0600

    schema validation

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,36 +1,47 @@
 // ? internal import
-const User = require("../models/userSchema")
+const User = require('../models/userSchema');
 
 // * get posts page
 
 // TODO: display posts
 const getSingIn = async (req, res) => {
   try {
-    const singIn = await User.find()
+    const singIn = await User.find();
 
-    res.status(200).json(singIn)
+    res.status(200).json(singIn);
   } catch (error) {
-    res.status(404).json({message: error.message})
+    res.status(404).json({ message: error.message });
   }
-}
-
+};
 
 // TODO: create post
 const createUser = async (req, res) => {
-  const post = req.body;
-  
-  const newUser = new User(post)
+  const { name, email, phone, work, password, cPassword } = req.body;
 
-  try {
-    await newUser.save()
-
-    res.status(201).json(newUser)
-  } catch (error) {
-    res.status(409).json({message: error.message})
+  if (!name || !email || !phone || !work || !password || !cPassword) {
+    return res.status(422).json({ error: 'plz, filled the field property' });
   }
-}
+
+  User.findOne({ email })
+    .then((userExit) => {
+      if (userExit) {
+        return res.status(422).json({ error: 'Email already exit!' });
+      }
+      const user = new User({ name, email, phone, work, password, cPassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: 'user registered successfully!' });
+        })
+        .catch((err) => res.status(500).json({ error: 'Failed to register!' }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   getSingIn,
   createUser,
-}
+};
