d3073ff4ba3b8f9c94106ef46e42d11acb5587ff
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-10 06:21:41 +0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <div>
-    <h1>RS Shonjoy</h1>
-  </div>
+  <>
+    <Navbar />
+  </>
 );
 
 export default App;
