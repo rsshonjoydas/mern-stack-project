0ac18129c4143ce2dad70022bb999070be170d57
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-10 06:58:08 +0600

    react router with navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Login from './components/Login';
+// import SignUp from './components/SignUp';
 
 const App = () => (
   <>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,55 +1,82 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Login from './Login';
+import SignUp from './SignUp';
 
 const Navbar = () => (
   <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="{#}">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="{#}navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link active" aria-current="page" href="{#}">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="{#}">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="{#}">
-                Contact
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="{#}">
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="{#}">
-                Registration
-              </a>
-            </li>
-          </ul>
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="/navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link active" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="contact" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="signup" className="nav-link">
+                  Registration
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
   </>
 );
 
