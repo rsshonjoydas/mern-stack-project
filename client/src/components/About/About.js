aeb43b6db3d606f9c462bf9517900452213b28de
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-10 22:12:15 +0600

    about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import profile from '../images/profile-1.jpeg';
+import './style.css';
 
 const About = () => (
-  <div>
-    <h1>About</h1>
-  </div>
+  <section className="about" id="about">
+    <div className="max-width">
+      <h2 className="title">About me</h2>
+      <div className="about-content">
+        <div className="column left">
+          <img src={profile} alt="" />
+        </div>
+        <div className="column right">
+          <div className="text">
+            I am Prakash and I am a <span className="typing-2" />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet
+            doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere
+            explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio
+            consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore
+            exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta
+            quas culpa voluptatum repudiandae harum non.
+          </p>
+          <a href="{#}">Download CV</a>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default About;
